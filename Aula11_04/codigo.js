b6d3d6836fb2bd8df065cdd354a6b2d73cf97689
Dionescha <100169965+Dionescha@users.noreply.github.com>
2022-04-11 22:30:33 -0300

    Add files via upload

---
+++ Aula11_04/codigo.js
@@ -0,0 +1,23 @@
+var aula = null
+
+console.log(typeof aula);
+
+
+var nome = ['Dione'];
+var idade = 18;
+var dmaior = true;
+var prof = ['Régis', 'Leandro', 'André', 'Fernanda', 'Elias'];
+
+var propriedades =
+
+nome = 'Dione'
+idade = 19
+nome = 'Juca'
+idade = 50
+
+var number1 = 50 
+var number2 = 52
+var soma = number1 + number2
+
+console.log(soma)
+
