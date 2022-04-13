50e7792c5bcea5ea71aed79b3ce0f4f0f1aae4ae
Dionescha <100169965+Dionescha@users.noreply.github.com>
2022-04-12 22:23:20 -0300

    Add files via upload

---
+++ Aula12_04/script.js
@@ -0,0 +1,26 @@
+
+function inserir() {
+    var button = document.createElement("button");
+    button.innerHTML = "Enviar";
+    button.setAttribute('onClick', 'showMessage()')
+    console.log(button)
+
+
+
+
+
+
+
+
+
+
+
+    
+    document.body.appendChild(button);
+}
+
+function showMessage() {
+    alert('Olá')
+    
+}
+
